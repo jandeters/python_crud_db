0f51146e5e918e9c5607ad4a84c642acfdbf6cae
Janice Deters <janice.deters@gmail.com>
2022-09-27 21:55:48 -0300

    Adicionado metodo pesquisar

--- AlunoRepository.py
+++ AlunoRepository.py
@@ -1,15 +1,25 @@
+# primeiro nome do arquivo e depois a classe
+from Aluno import Aluno
 class AlunoRepository():
+    
 
     def __init__(self, alunos=[]):
        self.alunos=alunos
 
-    def inserir(self,aluno):
+    def inserir(self,aluno:Aluno): 
         self.alunos.append(aluno)
 
+    def pesquisar(self,nome:str): 
+        lista=[]
+        for aluno in self.alunos:
+            if nome in aluno.nome:
+                lista.append(aluno)         
+        return lista
 
+        
     def listar(self):
-        print("------------ Lista de alunos  ------------")
+        print("------------------   Lista de alunos  ---------------------")
         for aluno in self.alunos:
             print(" Nome: {} Matricula: {} CPF: {} Endereço:  {} ".format(aluno.nome, aluno.matricula,aluno.cpf,aluno.endereco))
-        print("-------------------------------------")    
+        print("------------------=----------------------------------------")    
         
--- main.py
+++ main.py
@@ -3,25 +3,41 @@ from Aluno import Aluno
 
 #chama o gerenciador 
 repo=AlunoRepository()
-
+opcao=0
 print("Bem vindo ao Sistema Cleto School!")
-print('''[1] Incluir aluno
-[2] Pesquisar aluno
-[3] Alterar aluno
-[4] Excluir aluno ''')
-opcao = input("Escolha uma das opções: ")
-print("Opção escolhida: {}".format(opcao))
+
+while(opcao!=6):
+    print(''' 
+    ------  MENU ------- 
+    [1] Incluir aluno
+    [2] Pesquisar aluno
+    [3] Alterar aluno
+    [4] Excluir aluno 
+    [5] Listar
+    [6] Sair ''')
+    opcao = int(input("Escolha uma das opções: "))
+    print("Opção escolhida: {}".format(opcao))
 
 
-if(opcao=="1"):
-    print(" -------- CADASTRO DE ALUNOS ------- ")
-    nome=input("Informe nome: ")
-    matricula=input("Informe a matricula: ")
-    cpf = input("Informe o CPF: ")
-    endereco = input("Informe o endereco: ")
-    aluno=Aluno(nome,matricula,cpf,endereco)
-    
-    repo.inserir(aluno)
-    repo.listar()
+    if(opcao==1):
+        print(" -------- CADASTRO DE ALUNOS ------- ")
+        nome=input("Informe nome: ")
+        matricula=input("Informe a matricula: ")
+        cpf = input("Informe o CPF: ")
+        endereco = input("Informe o endereco: ")
+        aluno=Aluno(nome,matricula,cpf,endereco)
+        
+        repo.inserir(aluno)
+        repo.listar()
 
-    
+    if(opcao==2):
+       
+        nome2=input("Pesquisar pelo o nome: ")
+        saida=repo.pesquisar(nome2)
+        if not saida: 
+             print("Nenhum aluno encontrado com o nome:",nome2)
+        else: 
+            for aluno in saida:
+                print(" Nome: {} Matricula: {} CPF: {} Endereço:  {} ".format(aluno.nome, aluno.matricula,aluno.cpf,aluno.endereco))
+                print("------------------=----------------------------------------")    
+        
